6dd63451d329aad28ff21dcf5820a7017519eb3c
Ben Forest <100401981+mydropboxisfull@users.noreply.github.com>
2023-10-12 03:06:22 -0400

    added user error catches and UI updates

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,24 @@ import { googleLogout } from "@react-oauth/google";
 import ToggleableButton from "./components/ToggableButton";
 
 function App() {
-  // Moods
+
+
+    // ---------------- User mistakes ----------------------
+
+    const [noMood, setNoMood] = useState(false);
+    const [noActivity, setNoActivity] = useState(false);
+
+
+  // ---------------- MOOD ----------------------
   const moods = [
     { value: 1, emoji: "1.png" },
     { value: 2, emoji: "2.png" },
     { value: 3, emoji: "3.png" },
   ];
-  const [selectedMood, setSelectedMood] = useState(2);
+  const [selectedMood, setSelectedMood] = useState(null);
   const handleMoodSelect = (mood) => {
     setSelectedMood(mood.value);
+    setNoMood(false)
   };
 
   // ---------------- Activies ----------------------
@@ -41,60 +50,70 @@ function App() {
       });
   }, []);
 
-
-
-
-
   // ------- Button * Active Status * for Activities -------
 
   const [selectedActivities, setSelectedActivities] = useState({});
 
   const handleActivitySelect = (activity) => {
+    setNoActivity(false);
     console.log("the activity is", activity);
     setSelectedActivities((prevActivities) => {
-      console.log("the prev activities are:", prevActivities)
+      console.log("the prev activities are:", prevActivities);
       const updatedActivities = { ...prevActivities };
       updatedActivities[activity] = !updatedActivities[activity];
       return updatedActivities;
-      console.log("updated activities are:", updatedActivities)
     });
   };
 
+  // ---------------- Edit Activities ----------------------
 
-    //// test
+  const [editToggle, setEditToggle] = useState(false);
 
-    // const [isSelected, setIsSelected] = useState(false);
-  
-    // const handleClick = () => {
-    //   setIsSelected(!isSelected);
-    // };
+  const onEditToggle = () => {
+    setEditToggle(!editToggle);
+    setSelectedActivities([]);
+  };
 
-    // const handleButton = (activity) => {
-    //   handleClick();
-    //   handleActivitySelect(activity);
-    // }
+  // ---------------- Form Submission ----------------------
 
+  const handleSubmit = () => {
+    const hasCompletedActivity = Object.values(selectedActivities).some(activity => activity === true);
+    console.log("our new func", hasCompletedActivity)
 
+    // Check for user errors
 
-  
-    // ---------------- Form Submission ----------------------
+    if (selectedMood === null && hasCompletedActivity === false) {
+      setNoMood(true);
+      setNoActivity(true);
+    }
 
+    else if (selectedMood === null) {
+    setNoMood(true);
 
-    const handleSubmit = () => {
-      // Get an array of selected activities based on the keys of the object
-      const selectedActivityNames = Object.keys(selectedActivities);
+    }
+    else if (hasCompletedActivity === false) {
+      setNoActivity(true);
+      }
+
+    else {
     
-      // Map the selected activities to the activities array with completed status
-      const newActivities = activities.map((activity) => ({
-        name: activity,
-        completed: selectedActivityNames.includes(activity), // Check if it's selected
-      }));
     
-      const newDay = {
-        moodValue: selectedMood,
-        activities: newActivities,
-      };
-      
+
+
+    // Get an array of selected activities based on the keys of the object
+    const selectedActivityNames = Object.keys(selectedActivities);
+
+    // Map the selected activities to the activities array with completed status
+    const newActivities = activities.map((activity) => ({
+      name: activity,
+      completed: selectedActivityNames.includes(activity), // Check if it's selected
+    }));
+
+    const newDay = {
+      moodValue: selectedMood,
+      activities: newActivities,
+    };
+
     axios
       .post("/api/days", newDay)
       .then((response) => {
@@ -106,10 +125,11 @@ function App() {
 
     setSelectedActivities([]);
     setSelectedMood(null);
-  };
+  }};
 
-
-
+  // ------------------------------------------------------------------------
+  // -------------------------------HTML STUFFz-----------------------------------------
+  // ------------------------------------------------------------------------
 
   return (
     <div className="bg-white h-screen w-screen ">
@@ -131,7 +151,7 @@ function App() {
           />
         </svg>
       </div>
-             {/* ------------------------------------------------------------------------ */}
+      {/* ------------------------------------------------------------------------ */}
 
       <div className="flex content-center justify-center mt-10">
         <div className="bg-white shadow-sm border-[.4rem] border-black mx-[2rem] rounded-sm w-full max-w-lg">
@@ -156,11 +176,31 @@ function App() {
           </div>
 
           <div className="px-[3rem] bg-gray-200">
+            {/* ---------------------- MOOD ---------------------- */}
+            <h2 className="text-xl text-black mb-4 font-bold pt-[1.5rem]">
+              How u feeling pal?
+            </h2>
+            {noMood && (
+              <div className="flex justify-start pb-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="red"
+                  class="w-6 h-6"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
 
+                <p className="text-red-500 text-sm pb-2">
+                  You need to select a mood pal...
+                </p>
+              </div>
+            )}
 
-
-            {/* ----------- MOOD ----------- */}
-            <h2 className="text-xl text-black mb-4 font-bold pt-[1.5rem]">How u feeling today pal?</h2>
             <div className="mb-4 flex justify-center space-x-4">
               {moods.map((mood, index) => (
                 <button
@@ -177,32 +217,74 @@ function App() {
               ))}
             </div>
 
-            {/* ----------- Activities ----------- */}
-            <div>
+            {/* ---------------------- Activities ---------------------- */}
+            <div className="pb-6">
+              <h3 className="text-xl text-black mb-4 font-bold pt-[1rem]">
+                What did you do today?
+              </h3>
+              {noActivity && (
+              <div className="flex justify-start pb-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="red"
+                  class="w-6 h-6"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
 
-  <h3 className="text-xl text-black mb-4 font-bold pt-[1rem]">What did you do today?</h3>
-  <div className="space-x-2">
-  {activities.map((activity) => (
-          <ToggleableButton
-            key={activity}
-            label={activity}
-            isSelected={selectedActivities[activity]}
-            onClick={() => handleActivitySelect(activity)}
-          />
-        ))}
-  </div>
-</div>
+                <p className="text-red-500 text-sm pb-2">
+                  You need to select what you did today pal...
+                </p>
+              </div>
+            )}
+              <div className="justify-end flex my-2">
+                <button onClick={onEditToggle}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className={`w-6 h-6 ${
+                      editToggle ? "opacity-100" : "opacity-50"
+                    }`}
+                  >
+                    <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+                    <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+                  </svg>
+                </button>
+              </div>
 
-            {/* ----------- Button Event ----------- */}
+              <div className="space-x-2">
+                {activities.map((activity) => (
+                  <ToggleableButton
+                    key={activity}
+                    label={activity}
+                    isSelected={selectedActivities[activity]}
+                    onClick={() => handleActivitySelect(activity)}
+                    isBeingEdited={editToggle}
+                  />
+                ))}
+              </div>
+            </div>
 
-            <div className="mt-4 text-center">
+            {/* ---------------------- Button Event  ---------------------- */}
+          </div>
+          <div className="my-4 text-center">
+            <div className="flex justify-between">
+              <div className="px-[2rem]"></div>
+
               <button
                 onClick={handleSubmit}
-                className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+                Ac
+                className="bg-black hover:bg-gray-400 text-white py-3 w-full px-full px-[2rem] rounded-sm"
               >
                 Submit
               </button>
-
+              <div className="px-[2rem]"></div>
             </div>
           </div>
         </div>
--- src/components/ToggableButton.jsx
+++ src/components/ToggableButton.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function ToggleableButton({ label, onClick, isSelected }) {
+function ToggleableButton({ label, onClick, isSelected, isBeingEdited }) {
   return (
     <button
       onClick={onClick}
-      className={`px-2 py-2 mx-2 my-1 ${isSelected ? 'bg-black rounded-sm text-white font-bold opacity-100' : 'bg-black rounded-sm text-white font-bold opacity-50'}`}
+      className={`px-2 py-2 mx-2 my-1 ${isSelected || isBeingEdited ? 'bg-black rounded-sm text-white font-bold opacity-100' : 'bg-black rounded-sm text-white font-bold opacity-50'}`}
     >
       {label}
+      {isBeingEdited && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6 ml-2" // Add margin to separate the icon from the label
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      )}
     </button>
   );
 }
